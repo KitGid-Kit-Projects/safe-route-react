17dcb185ab5ca7f0f481c4457417bc6c4c95f0d0
Shahmirze <85016768+ShahmirzeMIT@users.noreply.github.com>
2025-08-08 15:05:16 +0000

    STEP-2

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,8 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
-import ProtectedRoute from "./routes/ProtectedRoute";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
-import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -25,14 +23,7 @@ const App = () => (
             <Route path="/signup" element={<Signup />} />
             
             {/* Protected Routes */}
-            <Route 
-              path="/home" 
-              element={
-                <ProtectedRoute>
-                  <Home />
-                </ProtectedRoute>
-              } 
-            />
+
             
             {/* Redirect root to home */}
             <Route path="/" element={<Navigate to="/home" replace />} />
